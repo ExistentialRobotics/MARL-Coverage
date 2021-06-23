6c90116e3a1148061cd3bfc9096e30afd60fc76c
Peter Stratton <peterstratton121@gmail.com>
2021-06-22 22:14:52 -0700

    added file and method headers

--- adaptive_coverage.py
+++ adaptive_coverage.py
@@ -1,6 +1,14 @@
+"""
+adaptive_coverage.py contains the adaptive coverage algorithm given in the
+paper: Decentralized, Adaptive Coverage Control for Networked Robots,
+environment variable parsing, and instanciation of the map and agents.
+
+Authors: Peter Stratton, Hannah Hui
+Emails: pstratto@ucsd.edu, hahui@ucsd.edu
+"""
+
 import numpy as np
 import json
-from scipy.spatial import Voronoi, voronoi_plot_2d, KDTree
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 from map import *
@@ -8,18 +16,48 @@ from agent import *
 from utils import *
 import matplotlib.colors as mcolors
 
-ERROR = 0.01
-DT = 0.001
 
-def conv_check(agents, a_opt, error=0.01):
-    for a in agents:
-        print(str(a.a_est) + " " + str(np.linalg.norm(a.a_est - a_opt)))
-        if np.linalg.norm(a.a_est - a_opt) > error:
-            return False
-    return True
+def adaptive_coverage(map, agents, a_opt, lr_gain, gain_const, gamma_const,
+                      data_weighting=None, DT=1, render_agents=False,
+                      iters=100):
+    """
+    adaptive_coverage implements the central algorithm produced by the paper:
+    Decentralized, Adaptive Coverage Control for Networked Robots. It controls a
+    group of agents to learn the distribution of sensing data in the environment
+    using only information local to each agent.
 
+    Parameters
+    ----------
+    map            : python object that represents the environment the agent
+                     takes action in
+    agents         : list containing the autonomous learners who's parameter
+                     updates constitute "learning" the environment
+    a_opt          : numpy array containing optimal parameter vector that the
+                     agents are trying to learn
+    lr_gain        : float reprenting the learning rate for agent parameter
+                     updates
+    gain_const     : float that scales the gain matrix, gain matrix is applied
+                     to control inputs
+    gamma_const    : float that scales the gamma matrix, gamma matrix is applied
+                     when projecting agent parameters
+    data_weighting : function to weight the data
+    DT             : float representing the time interval between each algorithm
+                     step
+    render_agents  : boolean to display the agents each iteration or not
+    iters          : int dictating the number of iterations to run the algorithm
+                     for
 
-def adaptive_coverage(map, agents, a_opt, lr_gain, gain_const, gamma_const, data_weighting, DT, render_agents=False, iters=100):
+    Returns
+    -------
+    est_dists  : list containing mean distance from the agents to their
+                 estimated centroids on each iteration
+    true_dists : list contain mean distance from agents to their true centroids
+                 on each iteration
+    """
+    # effectively have no data weighting if no function is provided
+    if data_weighting is None:
+        data_weighting = 1
+
     # init gain and gamma matricies
     gain_matrix = gain_const * np.eye(2)
     gamma_matrix = gamma_const * np.eye(a_opt.shape[0])
@@ -28,23 +66,24 @@ def adaptive_coverage(map, agents, a_opt, lr_gain, gain_const, gamma_const, data
     est_errors = []
     true_errors = []
 
-    # _ = 0
-    # while conv_check(agents, opt_a, error=ERROR) == False:
     for _ in range(iters):
+        if (_ + 1) % 5 == 0:
+            print("-----------------ITER: " + str(_ + 1) + "------------------")
+
         # potentially render agents
         if render_agents:
             plt.clf()
             # render areas that agents should be moving towards
-            plt.scatter(agents[0].means[0][0], agents[0].means[0][1], s=50, c='r', marker='x')
-            plt.scatter(agents[0].means[len(agents[0].means) - 1][0], agents[0].means[len(agents[0].means) - 1][1], s=50, c='r', marker='x')
+            plt.scatter(agents[0].means[0][0], agents[0].means[0][1], s=50,
+                        c='r', marker='x')
+            plt.scatter(agents[0].means[len(agents[0].means) - 1][0],
+                        agents[0].means[len(agents[0].means) - 1][1], s=50,
+                        c='r', marker='x')
             for agent in agents:
                 agent.render_self()
                 agent.render_centroid()
             plt.draw()
             plt.pause(0.02)
-
-        if (_ + 1) % 10 == 0:
-            print("---------------------ITER: " + str(_ + 1) + "----------------------")
 
         # reset KDTree used to compute Voronoi regions
         map.set_tree(agents)
@@ -63,7 +102,8 @@ def adaptive_coverage(map, agents, a_opt, lr_gain, gain_const, gamma_const, data
             # calc F
             F = -agent.calc_F(gain_matrix)
 
-            a_pre = (F @ agent.a_est) - lr_gain * (agent.la @ agent.a_est - agent.lb) # eq 13
+            a_pre = (F @ agent.a_est) - lr_gain * (agent.la @ agent.a_est -
+                     agent.lb) # eq 13
             I_proj = agent.calc_I(a_pre) # eq 15
             a_dot = a_pre - I_proj @ a_pre
             agent.a_est = agent.a_est + DT * gamma_matrix @ a_dot # eq 14
@@ -83,17 +123,6 @@ def adaptive_coverage(map, agents, a_opt, lr_gain, gain_const, gamma_const, data
         # average estimated and true position errors
         est_errors.append((est_mean / len(agents)))
         true_errors.append((true_mean / len(agents)))
-        # _ += 1
-
-    # render areas that agents should be moving towards
-    plt.scatter(agents[0].means[0][0], agents[0].means[0][1], s=50, c='r', marker='x')
-    plt.scatter(agents[0].means[len(agents[0].means) - 1][0], agents[0].means[len(agents[0].means) - 1][1], s=50, c='r', marker='x')
-    for agent in agents:
-        agent.render_self()
-        agent.render_centroid()
-
-    # plot final voronoi diagram
-    map.plot_voronoi(agents)
 
     return est_errors, true_errors
 
@@ -126,27 +155,39 @@ if __name__ == "__main__":
     DATA_WEIGHTING = env_vars["data_weighting"]
     POS_CONSENSUS_GAIN = np.array(env_vars["positive_consensus_gain"])
     POS_DIM = env_vars["pos_dim"]
+    DT = env_vars["dt"]
+    ITERS = env_vars["iters"]
+
+    if DATA_WEIGHTING == -1:
+        d_f = None
+    else:
+        d_f = DATA_WEIGHTING
 
     # set a (change this to be in config later)
     opt_a = np.array([100, 100])
-    # opt_a = np.array([100, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A, 100])
+    # opt_a = np.array([100, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A, MIN_A,
+    #                   100])
 
 
     cov_m = BASIS_SIGMA * np.eye(2)
     means = np.array([(25, 25), (75, 75)])
-    basis_f = [multivariate_normal(mean=np.array([25, 25]), cov=cov_m), multivariate_normal(mean=np.array([75, 75]), cov=cov_m)]
+    basis_f = [multivariate_normal(mean=np.array([25, 25]), cov=cov_m),
+               multivariate_normal(mean=np.array([75, 75]), cov=cov_m)]
 
-    # calc basis centers, assuming you want them in the center of each quadrant
+    # # calc basis centers assuming they're in the center of each quadrant
     # basis_f = []
     # means = []
     # for i in range(int(np.sqrt(NUM_BASIS_FX))):
-    #     y = (MAP_HEIGHT / (np.sqrt(NUM_BASIS_FX) * 2)) + i * (MAP_HEIGHT / np.sqrt(NUM_BASIS_FX))
+    #     y = (MAP_HEIGHT / (np.sqrt(NUM_BASIS_FX) * 2)) + i * (MAP_HEIGHT /
+    #          np.sqrt(NUM_BASIS_FX))
     #     for j in range(int(np.sqrt(NUM_BASIS_FX))):
-    #         x = (MAP_WIDTH / (np.sqrt(NUM_BASIS_FX) * 2)) + j * (MAP_WIDTH / np.sqrt(NUM_BASIS_FX))
+    #         x = (MAP_WIDTH / (np.sqrt(NUM_BASIS_FX) * 2)) + j * (MAP_WIDTH /
+    #              np.sqrt(NUM_BASIS_FX))
     #         means.append((x, y))
     #
     #         # add multivariate normals as each basis function
-    #         basis_f.append(multivariate_normal(mean=np.array([x, y]), cov=cov_m))
+    #         basis_f.append(multivariate_normal(mean=np.array([x, y]),
+    #                        cov=cov_m))
 
     # instantiate map
     map = Map(MAP_WIDTH, MAP_HEIGHT, GRID_CELL_SIZE)
@@ -154,16 +195,34 @@ if __name__ == "__main__":
     # create agents with random initial positions
     agents = []
     for i in range(NUM_AGENTS):
-        agents.append(Agent(np.random.randint(MAP_WIDTH), np.random.randint(MAP_HEIGHT), basis_f, means, MIN_A, opt_a, POS_DIM, color=colors[i]))
+        agents.append(Agent(np.random.randint(MAP_WIDTH),
+                            np.random.randint(MAP_HEIGHT), basis_f, means,
+                            MIN_A, opt_a, POS_DIM, color=colors[i]))
 
     # print agent coordinates for debugging purposes
     print_agent_coords(agents)
 
     # run adaptive coverage algorithm
-    est_errors, true_errors = adaptive_coverage(map, agents, opt_a, LR_GAIN, GAIN_CONST, GAMMA_CONST, DATA_WEIGHTING, DT, render_agents=True)
+    est_errors, true_errors = adaptive_coverage(map, agents, opt_a, LR_GAIN,
+                                                GAIN_CONST, GAMMA_CONST,
+                                                data_weighting=d_f, DT=DT,
+                                                render_agents=True, iters=ITERS)
+
+    # plot agent positions and means corresponding to highest a_opt values
+    plt.scatter(agents[0].means[0][0], agents[0].means[0][1], s=50, c='r',
+                marker='x')
+    plt.scatter(agents[0].means[len(agents[0].means) - 1][0],
+                agents[0].means[len(agents[0].means) - 1][1], s=50, c='r',
+                marker='x')
+    for agent in agents:
+        agent.render_self()
+        agent.render_centroid()
+
+    # plot final voronoi diagram
+    map.plot_voronoi(agents)
 
     # plot the dist from centroids per iteration
-    plt.figure(1)
+    plt.figure(2)
     plt.title("Dist from True and Estimated Centroids")
     plt.xlabel('Iterations')
     plt.ylabel('Dist')
--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
+"""
+utils.py contains debugging methods meann't to print certain values to the
+console window
+
+Authors: Peter Stratton, Hannah Hui
+Emails: pstratto@ucsd.edu, hahui@ucsd.edu
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def print_env_vars(env_vars):
+    """
+    print_env_vars prints the program's environment variables to the console
+    window.
+
+    Parameter
+    ---------
+    env_vars : dictionary of environment variables to print
+    """
     print("----------------------Printing Env Variables-----------------------")
     for e in env_vars:
         print(e + " " + str(env_vars[e]))
@@ -9,19 +25,42 @@ def print_env_vars(env_vars):
 
 
 def print_agent_coords(agents):
+    """
+    print_agent_coords prints each agent's position to the console window.
+
+    Parameter
+    ---------
+    agents : list of agents who's positions need printing
+    """
     print("-----------------------Printing Agent Coords-----------------------")
     for a in agents:
         print("x: " + str(a.pos[0]) + " y: " + str(a.pos[1]))
     print("-------------------------------------------------------------------")
 
 def print_agent_centroids(agents):
-    print("-----------------------Printing Agent Centroids-----------------------")
+    """
+    print_agent_centroids prints each agent's estimated centroid position to the
+    console window.
+
+    Parameter
+    ---------
+    agents : list of agents who's estimated centroid positions need printing
+    """
+    print("---------------------Printing Agent Centroids----------------------")
     for a in agents:
         print("x: " + str(a.e_centroid[0]) + " y: " + str(a.e_centroid[1]))
     print("-------------------------------------------------------------------")
 
 def print_agent_voronoi(agent):
-    print("-----------------------Printing Agent Voronoi Partitions-----------------------")
+    """
+    print_agent_voronoi prints an agent's grid cells corresponding to its
+    voronoi region to the console window.
+
+    Parameter
+    ---------
+    agent : agent who's voronoi partition is printed 
+    """
+    print("-----------------Printing Agent Voronoi Partitions-----------------")
     print("Agent: x = " + str(agent.pos[0]) + " y = " + str(agent.pos[1]))
     for v in agent.v_part_list:
         print("x: " + str(v[0]) + " y: " + str(v[1]))
