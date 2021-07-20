1bc724648066e1679057b06e4c69229b9a069d93
Shreyas <sharora@gmail.com>
2021-07-20 13:13:48 -0700

    added video saving of runs, need a better framework for logging

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # How to Run the Code
 
 - Clone the Repository
-- Create a virtual environment and install all dependencies
+- Create a virtual environment and install all dependencies in requirements.txt
+- Install ffmpeg for video logging
 - edit main.py to use the controller you want with the parameters you want
 - run main.py
+
+
+# Results
+
+- All results are currently located in the tests directory, grouped by controller
+- TODO: improve logging and generation of results
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as ani
 from Controllers.voronoi_controller import VoronoiController
 from Controllers.grid_controller import GridController
 from Controllers.ergodic_controller import ErgodicController
 from Environments.environment import Environment
 from Agents.swarm_agent import Swarm_Agent
+import os
 
 '''Environment Parameters'''
 numrobot = 6
@@ -20,10 +22,23 @@ res = (40,40) #resolution tells us how many regions to divide each axis into
 numbasis = 50
 colorlist = list(np.random.rand(numrobot))
 
+
+#logging parameters
+makevid = True
+testname = "ergodic_simple"
+output_dir = "./tests/{}/".format(testname)
+
+#checking if output directory exists and making it if it doesn't
+if os.path.isdir(output_dir):
+    print("directory already exists, overwriting previous test")
+else:
+    os.makedirs(output_dir)
+
+
 '''Making the Controller for the Swarm Agent'''
 # c = VoronoiController(numrobot, region, res, gain)
 # c = GridController(numrobot, region, res, gain)
-c = ErgodicController(numrobot, region, res, 50, dt, avoidobstacles=True)
+c = ErgodicController(numrobot, region, res, numbasis, dt, avoidobstacles=False)
 
 '''Making the Swarm Agent'''
 agents = [Swarm_Agent(numrobot, c, colorlist)]
@@ -32,8 +47,26 @@ agents = [Swarm_Agent(numrobot, c, colorlist)]
 e = Environment(agents, numrobot, numobstacles, obstradius, region, dt, seed)
 
 '''Simulating the environment forward and rendering'''
+
+#creating video writer
+if(makevid):
+    writer = ani.FFMpegWriter(fps= int(1/dt))
+    writer.setup(plt.gcf(), output_dir + testname + ".mp4", dpi=100)
+
 e.reset()
 numsteps = 1000
 for i in range(numsteps):
     e.step()
     e.render()
+
+    #code to log video of run
+    if(makevid):
+        writer.grab_frame()
+
+#saving video
+if(makevid):
+    writer.finish()
+
+
+
+
