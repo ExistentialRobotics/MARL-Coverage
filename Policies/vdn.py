9d115bf1ef3ccdb166f52fde34eecd9e75759123
Shreyas <sharora@gmail.com>
2021-08-26 22:03:36 -0700

    added vdn and it runs, awaiting results, seems to be a tad slower than dqn although network is the same size

--- Policies/vdn.py
+++ Policies/vdn.py
@@ -1,29 +1,34 @@
 import numpy as np
 import torch
+import itertools
 from . base_policy import Base_Policy
+from . Networks.grid_rl_conv import Grid_RL_Conv
+from . Networks.Qnet import Critic
 from . replaybuffer import ReplayBuffer
+from torch.distributions.categorical import Categorical
 from copy import deepcopy
 
 
-class DQN(Base_Policy):
+class VDN(Base_Policy):
 
-    def __init__(self, q_net, buff, num_actions, learning_rate, epsilon=0.999, min_epsilon=0.1,
+    def __init__(self, q_net, buff, numrobot, num_actions, learning_rate, epsilon=0.999, min_epsilon=0.1,
                  batch_size=100, gamma=0.99, tau=0.9, weight_decay=0.1,
                  model_path=None):
         super().__init__()
 
         #cpu vs gpu code
         self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+        # num actions per robot, artifact of old code
+        self.num_actions = num_actions
+        self.numrobot = numrobot
         # init q net
         self.q_net = q_net
-        self.num_actions = num_actions
 
         # init with saved weights if testing saved model
         if model_path is not None:
             self.q_net.load_state_dict(torch.load(model_path))
 
-        # init q target net
+        # init q net
         self.target_net = deepcopy(self.q_net)
 
         #moving nets to gpu
@@ -37,6 +42,7 @@ class DQN(Base_Policy):
         #optimizer
         self.optimizer = torch.optim.Adam(self.q_net.parameters(),
                                           lr=learning_rate, weight_decay=weight_decay)
+
         #epsilon-greedy parameters
         self._epsilon = 1
         self._e_decay = epsilon
@@ -59,42 +65,42 @@ class DQN(Base_Policy):
         This method takes the state as input and returns a list of actions, one
         for each robot.
         '''
+        # calc qvals, using no grad to avoid computing gradients
+        with torch.no_grad():
+            state_tensor = (torch.from_numpy(state).float()).to(self._device)
+            qvals = self.q_net(state_tensor)
 
         #epsilon greedy check
         s = np.random.uniform()
 
-        #epsilon greedy policy
-        #if we are testing then we use a smaller testing epsilon
-        if(s > self._epsilon or (self._testing and s > self._testing_epsilon)):
-            # calc qvals, using no grad to avoid computing gradients
-            with torch.no_grad():
-                state_tensor = (torch.from_numpy(state).float()).to(self._device)
-                qvals = self.q_net(state_tensor)
-            #greedy
-            u = torch.argmax(qvals)
-        else:
-            #random
-            u = np.random.randint(self.num_actions)
+        #generating the action for each robot
+        ulis = []
+        for i in range(self.numrobot):
+            #if we are testing then we use a smaller testing epsilon
+            if(s > self._epsilon or (self._testing and s>self._testing_epsilon)):
+                #greedy
+                u = torch.argmax(qvals[i * self.num_actions: (i + 1) * self.num_actions])
+                u = u.item()
+            else:
+                #random
+                u = np.random.randint(self.num_actions)
 
-        return u
+            #adding controls
+            ulis.append(u)
+        return ulis
 
     def update_policy(self, episode):
         #adding new data to buffer
         self._buff.addepisode(episode)
 
-        #decaying epsilon
-        self.decayEpsilon()
+        #decaying the epsilon
+        if self._epsilon > self._min_epsilon:
+            self._epsilon *= self._e_decay
 
-        #performing one gradient step per env step (like spinning up ddpg)
-        # for i in range(5):
-        self.update_policy_step()
-
-
-    def update_policy_step(self):
         #zero gradients
         self.optimizer.zero_grad()
 
-        #sampling batch from buffer
+        #sampling episodes
         states, actions, rewards, next_states, done = self._buff.samplebatch(self.batch_size)
 
         # convert to tensors
@@ -136,25 +142,24 @@ class DQN(Base_Policy):
         next_qvals = self.target_net(next_states)
 
         # calculate gradient for q function
-        with torch.no_grad():
-            next_q = torch.max(next_qvals, 1).values
-            y = rewards + self._gamma*(1-done)*next_q
-
-        #TODO vectorize this for loop
+        y = torch.zeros(batch_size).to(self._device)
         currq = torch.zeros(batch_size).to(self._device)
-        for j in range(qvals.shape[0]):
-            currq[j] = qvals[j, actions[j]]
+        for i in range(self.numrobot):
+            with torch.no_grad():
+                next_q = torch.max(next_qvals[:, i * self.num_actions: (i + 1) * self.num_actions], 1).values
+                y += self._gamma*(1-done)*next_q
+            q_temp = qvals[:, i * self.num_actions: (i + 1) * self.num_actions]
 
+            #TODO vectorize this for loop
+            for j in range(q_temp.shape[0]):
+                currq[j] += q_temp[j, actions[j, i]]
+
+        y += rewards
         #calculating mean squared error
         loss = ((y-currq)**2).mean()
         loss.backward()
 
         return loss
-
-    def decayEpsilon(self):
-        #decaying the epsilon
-        if self._epsilon > self._min_epsilon:
-            self._epsilon *= self._e_decay
 
     def set_train(self):
         '''
